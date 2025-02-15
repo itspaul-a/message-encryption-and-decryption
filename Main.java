8360e3d834fec754616d5a4d1b00f9ae174aa7f5
paul <paguiar@go.pasadena.edu>
2025-02-15 00:35:26 -0800

    added comments

--- Main.java
+++ Main.java
@@ -6,10 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main{
-
+    
+    //take the file as input and check that file exists
     public static String[] getInput() 
     {
-        String filePath = "file.txt";
+        String filePath = "file.txt"; // will change to user input later
         try 
 	{
             String[] letterArray = readFileToStringArray(filePath);
@@ -23,7 +24,8 @@ public class Main{
         }
     }
 
-
+    
+    //convert file to string array
     public static String[] readFileToStringArray(String filePath) throws IOException 
     {
         String content = new String(Files.readAllBytes(Paths.get(filePath)));
@@ -34,7 +36,7 @@ public class Main{
         }
         return letters.toArray(new String[0]);
     }
-
+    
     public static void printArray()
     {
 	    String[] arr = getInput();
