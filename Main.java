be0d46b28df8d39c52a6d72eff5adc2700c4ce25
Paul Aguiar <98577306+itspaul-a@users.noreply.github.com>
2025-02-16 00:11:58 -0800

    Update Main.java
    added encryption method

--- Main.java
+++ Main.java
@@ -90,6 +90,39 @@ public class Main{
 	
 
     }
+public static void testCipher()
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter message: ");
+        String message = scan.nextLine();
+
+        System.out.print("Enter password: ");
+        String pass = scan.nextLine();
+
+        int temp = 0;
+
+        char[] words = message.toCharArray();
+        List<Character> words2 = new ArrayList<>();
+
+        for(int i = 0; i < pass.length(); i++)
+        {
+            temp = i;
+            while(temp < words.length)
+            {
+                words2.add(words[temp]);
+                temp+=pass.length();
+            }
+        }
+
+        for(int i = 0; i < words2.size(); i++)
+        {
+            System.out.print(words2.get(i) + " ");
+        }
+
+        scan.close();
+   }
+   
 
     public static void main(String[] args)
     {
